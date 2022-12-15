4a1b18f1ab0d6810b5720e030feca4c8c3d4544b
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2022-12-15 15:15:30 +0100

    3 a

--- tri_selection.py
+++ tri_selection.py
@@ -2,17 +2,27 @@
 
 def tri_par_selection(lst):
 
+    affectation = 0
+    comparaison = 0
+
     for l in range(len(lst)):
 
-        min_i = l
+        min_i = l 
+        affectation += 1
         for i in range(l, len(lst)):
 
+            comparaison += 1
             if lst[i] < lst[min_i]:
+
                 
                 min_i = i
-        lst[l], lst[min_i] = lst[min_i], lst[l]
+                affectation += 1 
 
-    return lst
+        lst[l], lst[min_i] = lst[min_i], lst[l] 
+        affectation += 3
+    
+    operation = affectation + comparaison
+
+    return lst, operation
 
 print(tri_par_selection([2,7,1,0,8,1]))
-assert tri_par_selection([5, 3, 1, 4, 2]) == [1,2,3,4,5]
