2672506f3ede7e88dade7b0afd44ba387e94bb72
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2022-12-15 14:41:16 +0100

    simple

--- tri_a_bulle_optimisé.py
+++ tri_a_bulle_optimisé.py
@@ -1,16 +1,23 @@
 def tri_bulle(tableau):
+
     permutation = True
     passage = 0
+
     while permutation == True:
+
         permutation = False
         passage = passage + 1
+
         for en_cours in range(0, len(tableau) - passage):
+
             if tableau[en_cours] > tableau[en_cours + 1]:
+                
                 permutation = True
+
                 # On echange les deux elements
                 tableau[en_cours], tableau[en_cours + 1] = \
                 tableau[en_cours + 1],tableau[en_cours]
+
     return tableau
 
 print(tri_bulle([2,7,1,0,8,1]))
-assert tri_bulle([5, 3, 1, 4, 2]) == [1,2,3,4,5]
--- tri_selection.py
+++ tri_selection.py
@@ -1,10 +1,14 @@
 # Trouvez le minimum de la liste et placez-le à la fin de la section triée
 
 def tri_par_selection(lst):
+
     for l in range(len(lst)):
+
         min_i = l
         for i in range(l, len(lst)):
+
             if lst[i] < lst[min_i]:
+                
                 min_i = i
         lst[l], lst[min_i] = lst[min_i], lst[l]
 
