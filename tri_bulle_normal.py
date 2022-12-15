603f8bda380da5af7ebb51c20682d5c306c48e96
Julien-QTX <julien.quatravaux@live.fr>
2022-12-15 13:38:08 +0100

    MergE

---
+++ tri_bulle_normal.py
@@ -0,0 +1,13 @@
+def tri_bulle(tableau):
+    permutation = True
+    passage = 0
+    while permutation == True:
+        permutation = False
+        passage = passage + 1
+        for en_cours in range(0, len(tableau) - passage):
+            if tableau[en_cours] > tableau[en_cours + 1]:
+                permutation = True
+                # On echange les deux elements
+                tableau[en_cours], tableau[en_cours + 1] = \
+                tableau[en_cours + 1],tableau[en_cours]
+    return tableau
--- tri_selection.py
+++ tri_selection.py
@@ -1,14 +1,14 @@
-def tri_selection(tableau):
-    nb = len(tableau)
-    for en_cours in range(0,nb):    
-        plus_petit = en_cours
-        for j in range(en_cours+1,nb) :
-            if tableau[j] < tableau[plus_petit] :
-                plus_petit = j
-        if min is not en_cours :
-            temp = tableau[en_cours]
-            tableau[en_cours] = tableau[plus_petit]
-            tableau[plus_petit] = temp
+# Trouvez le minimum de la liste et placez-le à la fin de la section triée
 
+def tri_par_selection(lst):
+    for l in range(len(lst)):
+        min_i = l
+        for i in range(l, len(lst)):
+            if lst[i] < lst[min_i]:
+                min_i = i
+        lst[l], lst[min_i] = lst[min_i], lst[l]
 
-            
+    return lst
+
+print(tri_par_selection([2,7,1,0,8,1]))
+assert tri_par_selection([5, 3, 1, 4, 2]) == [1,2,3,4,5]&
