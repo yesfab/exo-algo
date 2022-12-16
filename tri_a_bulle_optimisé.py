a1e9842b96652c213d09294f28dda39ff12c24c7
adamboissy2004@gmail.com <adamboissy2004@gmail.com>
2022-12-16 09:30:43 +0100

    opti a et b finito

--- tri_a_bulle_optimisé.py
+++ tri_a_bulle_optimisé.py
@@ -1,23 +1,40 @@
 def tri_bulle(tableau):
 
+    affectation = 0
+    comparaison = 0
+    compteur = 0
+
     permutation = True
     passage = 0
 
+    affectation += 1
     while permutation == True:
 
         permutation = False
+        affectation += 1
+
         passage = passage + 1
+        affectation += 1
 
         for en_cours in range(0, len(tableau) - passage):
-
+            
+            comparaison += 1
             if tableau[en_cours] > tableau[en_cours + 1]:
+
+                compteur += 1
                 
                 permutation = True
+                affectation +=1
 
                 # On echange les deux elements
                 tableau[en_cours], tableau[en_cours + 1] = \
                 tableau[en_cours + 1],tableau[en_cours]
+                affectation += 1
 
-    return tableau
+    operation = affectation + comparaison
 
-print(tri_bulle([2,7,1,0,8,1]))
+    return tableau, operation , compteur
+
+print(tri_bulle([1, 2 ,3,  4 ,5 ,6,7,8,9,10]))
+print(tri_bulle([10,9,8,7,6,5,4,3,2,1]))
+
