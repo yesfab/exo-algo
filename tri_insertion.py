b4c4064b42b615a44c4d0af6a9e9d7fb275b3904
fabien <fabien.arnal@edu.esiee-it.fr>
2022-12-16 15:15:32 +0100

    update affectation/comparaison insertion

--- tri_insertion.py
+++ tri_insertion.py
@@ -1,20 +1,26 @@
 # Programme Python pour l'implémentation du tri par insertion
 def tri_insertion(tab): 
     # Parcours de 1 à la taille du tab
+    affectation = 0
+    comparaison = 0
     for i in range(1, len(tab)): 
         k = tab[i] 
         j = i-1
+        affectation += 1
         while j >= 0 and k < tab[j] : 
                 tab[j + 1] = tab[j] 
                 j -= 1
+                comparaison += 1
         tab[j + 1] = k
+        affectation += 1
+
+    operation = affectation + comparaison
+
+    return tab,operation
 
 #partie test
 tab = [5, 3, 1, 4, 2]
-tri_insertion(tab) 
-print ("Le tableau trié est:")
-for i in range(len(tab)): 
-    print(tab[i])
+print(tri_insertion([5, 3, 1, 4, 2, 10])) 
 
 """
 PROCEDURE tri_Insertion ( Tableau t)
